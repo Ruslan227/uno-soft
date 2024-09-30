2610cb98458ce3b0d42af910d8f86f5bcedf44b0
Rus <aliev.345@yandex.ru>
2024-09-30 11:14:10 +0300

    Add `Main` to launch app.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,47 @@
 package org.example;
 
+import org.example.external.sort.ExternalFileSorter;
+import org.example.transformers.GroupAggregator;
+import org.example.transformers.MatrixTransposer;
+import org.example.transformers.ValidLineFilter;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        var correctFileWriterOutputPath = Path.of("src", "main", "resources", "output_correct_writer.txt");
+        var correctFileWriter = new ValidLineFilter(
+                Path.of("src", "main", "resources", "lng-4.txt"),
+                correctFileWriterOutputPath
+        );
+        correctFileWriter.filter();
+
+        var duplicateRemovalOutputPath = Path.of("src", "main", "resources", "output_remove_duplicates.txt");
+        ExternalFileSorter.removeDuplicates(correctFileWriterOutputPath, duplicateRemovalOutputPath);
+
+        Files.delete(correctFileWriterOutputPath);
+
+
+        var matrixTransposerWriterOutputPath = Path.of("src", "main", "resources", "output_matrix_transposer.txt");
+        var matrixTransposerWriter = new MatrixTransposer(
+                duplicateRemovalOutputPath,
+                matrixTransposerWriterOutputPath,
+                correctFileWriter.fileInfo()
+        );
+        matrixTransposerWriter.transpose();
+
+        var output = Path.of("src", "main", "resources", "final_out.txt");
+        var groupAggregator = new GroupAggregator(
+                matrixTransposerWriterOutputPath,
+                output,
+                correctFileWriter.fileInfo(),
+                duplicateRemovalOutputPath
+        );
+        groupAggregator.aggregateGroups();
+
+        Files.delete(matrixTransposerWriterOutputPath);
+        Files.delete(duplicateRemovalOutputPath);
     }
 }
